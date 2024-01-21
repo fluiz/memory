859331f9bf9b83e23870739bf96c348027cfec76
Fabio Luiz <luiz.fabio@gmail.com>
2024-01-21 16:57:03 -0300

    Callback and curtains

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,64 @@ fn main() {
 }
 
 slint::slint! {
+    component MemoryTile inherits Rectangle {
+        callback clicked;
+        in property <bool> open-curtain;
+        in property <bool> solved;
+        in property <image> icon;
+
+        width: 64px;
+        height: 64px;
+        background: solved ? #34cE57 : #3960D5;
+        animate background { duration: 800ms; }
+        
+        Image {
+            source: icon;
+            width: parent.width;
+            height: parent.height;
+        }
+
+        // Left curtain
+        Rectangle {
+            background: #193076;
+            x: 0px;
+            width: open-curtain ? 0px : (parent.width / 2);
+            height: parent.height;
+            animate width {
+                 duration: 250ms;
+                 easing: ease-in;
+            }
+        }
+
+        // Right curtain
+        Rectangle {
+            background: #193076;
+            x: open-curtain ? parent.width : (parent.width / 2);
+            width: open-curtain ? 0px : (parent.width / 2);
+            height: parent.height;
+            animate width {
+                 duration: 250ms;
+                 easing: ease-in;
+            }
+            animate x {
+                 duration: 250ms;
+                 easing: ease-in;
+            }
+        }
+
+        TouchArea {
+            clicked => {
+                root.clicked();
+            }
+        }
+    }
+
     export component MainWindow inherits Window {
-        Text {
-            text: "hello world";
-            color: green;
+        MemoryTile {
+            icon: @image-url("icons/bus.png");
+            clicked => {
+                self.open-curtain = !self.open-curtain;
+            }
         }
     }
 }
